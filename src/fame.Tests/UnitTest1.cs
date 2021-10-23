40743e0fbafcb56b40adcdfdc5e5c974fd936fff
Matt Stovall <stovall88@hotmail.com>
2021-10-22 22:05:09 -0500

    init

---
+++ src/fame.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace fame.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ src/fame/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace fame
+{
+    public class Class1
+    {
+    }
+}
